2f284214eb92c854d679c1fb4d01b58411335993
zejian <lizejian7@gmail.com>
2017-10-30 09:01:50 +0800

    反转链表

---
+++ 24_反转链表.cpp
@@ -0,0 +1,24 @@
+// 面试题24：反转链表
+// 题目：定义一个函数，输入一个链表的头结点，反转该链表并输出反转后链表的
+// 头结点。
+ListNode* ReverseList(ListNode* pHead)
+{
+	//specical case
+	if (pHead == nullptr)
+		return nullptr;
+	ListNode* pReverseHead;
+	ListNode* pNode = pHead;
+	ListNode* pPrev = nullptr;
+	while (pNode != nullptr)
+	{
+		ListNode* pNext = pNode->next;
+		if (pNext == nullptr)
+			pReverseHead = pNode;
+		pNode->next = pPrev;
+		pPrev = pNode;
+		pNode = pNext;
+	}
+	return pReverseHead;
+}
+
+//递归事先
